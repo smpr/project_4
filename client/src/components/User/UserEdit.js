15192b40fd0c277fa66cf9ac203f1caab5eddb51
smpr <gcbrown1116@gmail.com>
2017-11-15 11:01:42 -0500

    user edit fixed

--- client/src/components/User/UserEdit.js
+++ client/src/components/User/UserEdit.js
@@ -2,14 +2,50 @@ import React, { Component } from 'react';
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 import { Redirect } from 'react-router-dom'
+import styled from 'styled-components'
+
+const BodyContainer = styled.div`
+display:flex;
+justify-content: space-around;
+background-color: #323232;
+height: 100vh;
+`
+const LinkDiv = styled.div`
+display:flex;
+justify-content: space-between;
+`
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: space-around;
+align-content: center;
+`
+const FormContainer = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-content: center;
+width: 30vw;
+color: white;
+background-color: #4B4B4B;
+a {
+    color: white;
+    text-decoration: none;
+}
+
+`
+
+const Button = styled.button`
+color: red;
+`
 class UserEdit extends Component {
-    state = { 
+    state = {
         info: {
-        city: "",
-        state: "",
-        address: "",
-        country: "",
-        zip: "",
+            city: "",
+            state: "",
+            address: "",
+            country: "",
+            zip: "",
         },
         redirectToInfoHome: false
     }
@@ -43,34 +79,38 @@ class UserEdit extends Component {
     handleSubmit = async (event) => {
         event.preventDefault()
         const res = await axios.post(`/api/infos`, { 'info': this.state.info })
-        this.setState({ redirectToHome: true})
+        this.setState({ redirectToHome: true })
 
     }
     render() {
         if (this.state.redirectToInfoHome) {
             return <Redirect to={`/Users/Home`} />
-          }
+        }
         return (
-            <div>
-                <br />
-                <h2><b>Edit User</b></h2>
-                <br />
-                Address: <input onChange={this.handleChange} name="address" value={this.state.info.address} />
-
-                <div>
-                    City: <input onChange={this.handleChange} name="city" value={this.state.info.city} />
-                </div>
-                <div>
-                    State:<input onChange={this.handleChange} name="state" value={this.state.info.state} />
-                </div>
-                <div>
-                    Zip:<input onChange={this.handleChange} name="zip" value={this.state.info.zip} />
-                </div>
-                <div>
-                    Country:<input onChange={this.handleChange} name="country" value={this.state.info.country} />
-                </div>
-                <button onClick={this.editInfo}>Edit</button>
-            </div>
+            <BodyContainer>
+                <FormContainer>
+                    <h2>Edit User</h2>
+                    
+                    <div>
+                        Address: <input onChange={this.handleChange} name="address" value={this.state.info.address} />
+                    </div>
+                    <div>
+                        City: <input onChange={this.handleChange} name="city" value={this.state.info.city} />
+                    </div>
+                    <div>
+                        State:<input onChange={this.handleChange} name="state" value={this.state.info.state} />
+                    </div>
+                    <div>
+                        Zip:<input onChange={this.handleChange} name="zip" value={this.state.info.zip} />
+                    </div>
+                    <div>
+                        Country:<input onChange={this.handleChange} name="country" value={this.state.info.country} />
+                    </div>
+                    <div>
+                        <button onClick={this.editInfo}>Edit</button>
+                    </div>
+                </FormContainer>
+            </BodyContainer >
         );
     }
 }
