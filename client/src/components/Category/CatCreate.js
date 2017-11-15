26a0cb93cf77a3fc277e975f83395510f789f7cb
smpr <gcbrown1116@gmail.com>
2017-11-15 10:24:08 -0500

    create category given style

--- client/src/components/Category/CatCreate.js
+++ client/src/components/Category/CatCreate.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
 import axios from 'axios'
 import { Redirect } from 'react-router-dom'
+import styled from 'styled-components'
 
+const BodyContainer = styled.div`
+display:flex;
+justify-content: space-around;
+background-color: #323232;
+height: 100vh;
+`
+const LinkDiv = styled.div`
+display:flex;
+justify-content: space-between;
+`
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: space-around;
+align-content: center;
+`
+const FormContainer = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: space-around;
+align-content: center;
+width: 25vw;
+color: white;
+background-color: #4B4B4B;
+a {
+    color: white;
+    text-decoration: none;
+}
+
+`
+
+const Button = styled.button`
+color: red;
+`
 class CatCreate extends Component {
     state = {
         category: [
-            
+
         ],
         redirectToHome: false,
     }
@@ -16,7 +51,7 @@ class CatCreate extends Component {
             ...this.state.category
         }
         updateCategory[event.target.name] = event.target.value
-        this.setState({ category: updateCategory})
+        this.setState({ category: updateCategory })
     }
     // this handles the submit function specifically setup on the back end to allow this route directly
     handleSubmit = async (event) => {
@@ -26,29 +61,34 @@ class CatCreate extends Component {
 
     }
     render() {
-        
+
         if (this.state.redirectToHome) {
             return <Redirect to="/categories" />
         }
         return (
-            <div>
-                Category Create Page
+            <BodyContainer>
                 <form onSubmit={this.handleSubmit}>
-                        <h2>New Category</h2>
-                        <input
-                            placeholder='Category Name'
-                            onChange={this.handleChange}
-                            name="title"
-                            type="text"
-                            required
-                           
-                            value={this.state.category.title} />
+                    <FormContainer>
+                        <div>
+                            <h2>New Category</h2>
+                        </div>
+                        <div>
+                            <input
+                                placeholder='Category Name'
+                                onChange={this.handleChange}
+                                name="title"
+                                type="text"
+                                required
+
+                                value={this.state.category.title} />
+                        </div>
                         <div>
                             <button>Create Category</button>
                         </div>
-                    </form>
-                    
-            </div>
+                    </FormContainer>
+                </form>
+
+            </BodyContainer>
         );
     }
 }
