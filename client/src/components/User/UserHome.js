09c1c704088fe5c573904b4f27fa2921b23edde1
smpr <gcbrown1116@gmail.com>
2017-11-15 10:30:18 -0500

    userhome styled

--- client/src/components/User/UserHome.js
+++ client/src/components/User/UserHome.js
@@ -1,6 +1,42 @@
 import React, { Component } from 'react';
 import { Link, Redirect } from 'react-router-dom'
 import axios from 'axios'
+import styled from 'styled-components'
+
+const BodyContainer = styled.div`
+display:flex;
+justify-content: space-around;
+background-color: #323232;
+height: 100vh;
+`
+const LinkDiv = styled.div`
+display:flex;
+justify-content: space-between;
+`
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: space-around;
+align-content: center;
+`
+const FormContainer = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: space-around;
+align-content: center;
+width: 25vw;
+color: white;
+background-color: #4B4B4B;
+a {
+    color: white;
+    text-decoration: none;
+}
+
+`
+
+const Button = styled.button`
+color: red;
+`
 class UserHome extends Component {
     state = {
         info: {
@@ -23,19 +59,23 @@ class UserHome extends Component {
 
     }
     render() {
- 
+
         return (
-            <div>
-                <div>User Info</div>
-                <div>First Name:</div>
-                <div>Last Name:</div>
-                <div>Street: {this.state.info.address}</div>
-                <div>City: {this.state.info.city}</div>
-                <div>State: {this.state.info.state}</div>
-                <div>Country: {this.state.info.country}</div>
-                <div>Zip: {this.state.info.zip}</div>
-                <div><Link to='/Users/edit'><button>Edit</button></Link></div>
-            </div>
+            <BodyContainer>
+                <FormContainer>
+                    <div>
+                        <h2>User Info</h2>
+                        First Name:<br />
+                        Last Name:<br />
+                        Street: {this.state.info.address}<br />
+                        City: {this.state.info.city}<br />
+                        State: {this.state.info.state}<br />
+                        Country: {this.state.info.country}<br />
+                        Zip: {this.state.info.zip}<br />
+                    </div>
+                    <div><Link to='/Users/edit'><button>Edit</button></Link></div>
+                </FormContainer>
+            </BodyContainer>
         );
     }
 }
