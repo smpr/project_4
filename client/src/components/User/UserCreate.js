85f2b161402245a00c2972100054e10f77c1e681
smpr <gcbrown1116@gmail.com>
2017-11-16 16:54:24 -0500

    user create now meets all styling standards

--- client/src/components/User/UserCreate.js
+++ client/src/components/User/UserCreate.js
@@ -68,115 +68,145 @@ class UserCreate extends Component {
         }
 
     }
-    promptToSecondForm = (event) => {
-        this.setState({
-            togglePage2: true
-        })
-
-    }
-    promptToThirdForm = (event) => {
-        this.setState({
-            togglePage2: false,
-            togglePage3: true
-        })
-
-    }
-    promptToFourthForm = (event) => {
-        this.setState({
-            togglePage3: false,
-            togglePage4: true
-        })
-
-    }
-
-    handleChange = (event) => {
-        const attribute = event.target.name
-        const clonedInfo = { ...this.state.info }
-        clonedInfo[attribute] = event.target.value
-        this.setState({ info: clonedInfo })
-    }
-    handleSubmit = async (event) => {
-        event.preventDefault()
-        const res = await axios.post(`/api/infos`, this.state.info)
-        this.setState({ togglePage4:false, togglePage6: true })
-        console.log("submit hit")
-
-    }
-    signUp = (event) => {
-        event.preventDefault()
-        this.props.signUp(
-            this.state.info.email,
-            this.state.info.password,
-            this.state.info.password_confirmation
-        ),
+        promptToSecondForm = (event) => {
             this.setState({
                 togglePage2: true
             })
-    }
 
-    render() {
-        const page1 =
-            <BodyContainer>
-                <FormContainer>
-                <div>
-                    <h2><b>Create User</b></h2>
-                </div>
-                <div>
-                    E-mail:
-                        <input onChange={this.handleChange} type="text" name="email" value={this.state.info.email} />
-                </div>
-                <div>
-                    Password:
-                        <input onChange={this.handleChange} type="password" name="password" value={this.state.info.password} />
-                </div>
-                <div>
-                    Confirm Password:
-                        <input onChange={this.handleChange} type="password" name="password_confirmation"
-                        value={this.state.info.password_confirmation} />
-                </div>
-                <div><button onClick={this.signUp}>Next</button></div>
-                </FormContainer>
+        }
+        promptToThirdForm = (event) => {
+            this.setState({
+                togglePage2: false,
+                togglePage3: true
+            })
+
+        }
+        promptToFourthForm = (event) => {
+            this.setState({
+                togglePage3: false,
+                togglePage4: true
+            })
+
+        }
+
+        handleChange = (event) => {
+            const attribute = event.target.name
+            const clonedInfo = { ...this.state.info }
+            clonedInfo[attribute] = event.target.value
+            this.setState({ info: clonedInfo })
+        }
+        handleSubmit = async (event) => {
+            event.preventDefault()
+            const res = await axios.post(`/api/infos`, this.state.info)
+            this.setState({ togglePage4:false, togglePage6: true })
+            console.log("submit hit")
+
+        }
+        signUp = (event) => {
+            event.preventDefault()
+            this.props.signUp(
+                this.state.info.email,
+                this.state.info.password,
+                this.state.info.password_confirmation
+            ),
+                this.setState({
+                    togglePage2: true
+                })
+        }
+
+        render() {
+            const page1 =
+                <BodyContainer>
+                    <Container>
+                        <FormContainer>
+                            <div>
+                                <h2><b>Create User</b></h2>
+                            </div>
+                            <div>
+                                E-mail:
+                                    <input onChange={this.handleChange} type="text" name="email" value={this.state.info.email} />
+                            </div>
+                            <div>
+                                Password:
+                                    <input onChange={this.handleChange} type="password" name="password" value={this.state.info.password} />
+                            </div>
+                            <div>
+                                Confirm Password:
+                                    <input onChange={this.handleChange} type="password" name="password_confirmation"
+                                    value={this.state.info.password_confirmation} />
+                            </div>
+                            <div>
+                                <button onClick={this.signUp}>Next</button>
+                            </div>
+                        </FormContainer>
+                </Container>
             </BodyContainer>
         const page2 =
             <BodyContainer>
-                <FormContainer>
-                <div>
-                    Address: <input onChange={this.handleChange} name="address" value={this.state.info.address} />
-                </div>
-                <div>
-                    Zip:<input onChange={this.handleChange} name="zip" value={this.state.info.zip} />
-                </div>
-                <div><button onClick={this.promptToThirdForm}>Next</button></div>
-                </FormContainer>
+                <Container>
+                    <FormContainer>
+                        <div>
+                            Address: <input onChange={this.handleChange} name="address" value={this.state.info.address} />
+                        </div>
+                        <div>
+                            Zip:<input onChange={this.handleChange} name="zip" value={this.state.info.zip} />
+                        </div>
+                        <div>
+                            <button onClick={this.promptToThirdForm}>Next</button>
+                        </div>
+                    </FormContainer>
+                </Container>
             </BodyContainer>
         const page3 =
             <BodyContainer>
-                <FormContainer>
-                <div>
-                    City: <input onChange={this.handleChange} name="city" value={this.state.info.city} />
-                </div>
-                <div>
-                    State:<input onChange={this.handleChange} name="state" value={this.state.info.state} />
-                </div>
+                <Container>
+                    <FormContainer>
+                        <div>
+                            City: <input onChange={this.handleChange} name="city" value={this.state.info.city} />
+                        </div>
+                        <div>
+                            State:<input onChange={this.handleChange} name="state" value={this.state.info.state} />
+                        </div>
 
-                <div>
-                    Country:<input onChange={this.handleChange} name="country" value={this.state.info.country} />
-                </div>
-                <div><button onClick={this.promptToFourthForm}>Next</button></div>
-                </FormContainer>
+                        <div>
+                            Country:<input onChange={this.handleChange} name="country" value={this.state.info.country} />
+                        </div>
+                        <div>
+                            <button onClick={this.promptToFourthForm}>Next</button>
+                        </div>
+                    </FormContainer>
+                </Container>
             </BodyContainer>
         const page4 =
             <BodyContainer>
-                <FormContainer>
-                <div><h2>Please Verify Your Info</h2></div>
-                <div>Email: {this.state.info.email}</div>
-                <div>Address: {this.state.info.address}</div>
-                <div>City: {this.state.info.city}</div>
-                <div>State: {this.state.info.state}</div>
-                <div>Zip: {this.state.info.zip}</div>
-                <div>Country: {this.state.info.country}</div>
-                <div><button onClick={this.handleSubmit}>Submit</button></div>
-                </FormContainer>
+                <Container>
+                    <FormContainer>
+                        <div>
+                            <h2>Please Verify Your Info</h2>
+                        </div>
+                        <div>
+                            Email: {this.state.info.email}
+                        </div>
+                        <div>
+                            Address: {this.state.info.address}
+                        </div>
+                        <div>
+                            City: {this.state.info.city}
+                        </div>
+                        <div>
+                            State: {this.state.info.state}
+                        </div>
+                        <div>
+                            Zip: {this.state.info.zip}
+                        </div>
+                        <div>
+                            Country: {this.state.info.country}
+                        </div>
+                        <div>
+                            <button onClick={this.handleSubmit}>Submit</button>
+                        </div>
+                    </FormContainer>
+                </Container>
             </BodyContainer>
         
         const moveAlong =
